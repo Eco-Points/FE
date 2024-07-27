0c1e5f7d895b88766964dada12a45012d215029d
Henryrivardo07 <henrysihombing20@gmail.com>
2024-07-27 21:14:18 +0700

    feature: integrasi edit profile

---
+++ src/function.ts
@@ -0,0 +1,34 @@
+export const checkProperty = (property: any) => {
+  if (property instanceof File) {
+    if (property.size !== 0) return true;
+  }
+
+  if (typeof property === "string") {
+    if (property !== "") return true;
+  }
+
+  if (typeof property === "boolean") {
+    return true;
+  }
+
+  if (typeof property === "number") {
+    return true;
+  }
+
+  return false;
+};
+
+export const valueFormatData = (value: any) => {
+  if (value instanceof File) {
+    return value as Blob;
+  }
+
+  return String(value);
+};
+
+export const formatCurrency = (number: number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  }).format(number);
+};
--- src/utils/types/users.ts
+++ src/utils/types/users.ts
@@ -1,3 +1,30 @@
+import { z } from "zod";
+import { MAX_MB, MAX_UPLOAD_SIZE, ACCEPTED_IMAGE_TYPES } from "@/const";
+
+export const editProfileSchema = z.object({
+  fullname: z.string().min(1, { message: "Full name is required" }),
+  email: z.string().min(1, { message: "Email is Required" }).email({ message: "Invalid Email" }),
+  phone: z.string().min(1, { message: "Phone number is Required" }),
+  address: z.string().min(1, { message: "Addres is Invalid" }),
+  password: z.string().min(6, "Password harus memiliki minimal 6 karakter"),
+  profile_picture: z
+    .instanceof(File)
+    .refine((file) => !file || file.size <= MAX_UPLOAD_SIZE, `Max Image size is ${MAX_MB} MB`)
+    .refine((file) => !file || file.type === "" || ACCEPTED_IMAGE_TYPES.includes(file.type), "Only .jpg, .jpeg, and .png format are supported"),
+});
+
+export type EditProfileSchema = z.infer<typeof editProfileSchema>;
+
+export interface EditProfileType {
+  fullname: string;
+  email: string;
+  password?: string;
+  phone_number: number;
+  address: string;
+  message?: string;
+  profile_picture: FileList;
+}
+
 export interface ProfileType {
   id: number;
   fullname: string;
